cc4406ca418981e17d5b3e912f0b035d4e387575
Daniel Sousa <daniel.s@crowdbotics.com>
2023-10-18 14:55:08 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "muddy-meadow-2",
+  url: "https://muddy-meadow-2.botics.co",
+  api: "https://muddy-meadow-2.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
